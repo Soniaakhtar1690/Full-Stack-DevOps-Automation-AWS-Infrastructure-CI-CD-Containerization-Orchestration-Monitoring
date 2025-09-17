683896088b54bf34a212ece86d0699e3472ed102
SONIA AKHTAR <143588840+Soniaakhtar1690@users.noreply.github.com>
2025-09-17 10:38:18 +0100

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,50 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+  tags = { Name = "${var.project_name}-igw" }
+}
+
+# public subnets
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = { Name = "${var.project_name}-public-${count.index + 1}" }
+}
+
+# private subnets
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = { Name = "${var.project_name}-private-${count.index + 1}" }
+}
+
+# Route Table for public subnets
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "${var.project_name}-public-rt" }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
